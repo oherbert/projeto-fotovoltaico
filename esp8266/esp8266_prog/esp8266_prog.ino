e29da70b1fa7ac282d4730e1d739bc6278ca99cd
oherbert <o_herbert@hotmail.com>
2023-03-19 12:59:20 -0300

    alterações do servidor e frontend

--- esp8266/esp8266_prog/esp8266_prog.ino
+++ esp8266/esp8266_prog/esp8266_prog.ino
@@ -2,30 +2,48 @@
 #include <ArduinoWebsockets.h>
 #include <ESP8266WiFi.h>
 
-const char* ssid = "NET_2GAC791F_EXT_2.4G";  //Enter SSID  casa NET_2GAC791F_EXT_2.4G
-const char* password = "DDAC791F";           //Enter Password - casa DDAC791F
+const char *ssid = "NET_2GAC791F_EXT_2.4G"; // Enter SSID  casa NET_2GAC791F_EXT_2.4G
+const char *password = "DDAC791F";          // Enter Password - casa DDAC791F
 
-const char* websockets_server_host = "192.168.0.6";  //Enter server adress
-const uint16_t websockets_server_port = 3333;        // Enter server port
+const char *websockets_server_host = "192.168.0.6"; // Enter server adress
+const uint16_t websockets_server_port = 3333;       // Enter server port
 
-float leitura;
+const char *TRUE = "true" const char *FALSE = "false"
+
+    float leitura;
 bool ionizador = false;
 
 using namespace websockets;
 
 WebsocketsClient client;
 
-void tryConnect() {
+void tryConnect()
+{
   bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
-  if (connected) {
+  if (connected)
+  {
     Serial.println("Cliente Esp conectado!");
     client.send("Olá Server: ESP8266 conectando.");
-  } else {
+  }
+  else
+  {
     Serial.println("Não conectado");
   }
 }
+void updateServer()
+{
+  if ionizador
+  {
+    client.send("update: {ionizador:{ph:" + 2 + ", output:" + String(leitura) + ", autoStart:{on:false,minValue:0,maxValue:0}}}");
+  }
+  else
+  {
+    client.send("update: {ionizador:{ph:" + 2 + ", output:" + String(leitura) + ", autoStart:{on:false,minValue:0,maxValue:0}}}");
+  }
+}
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   pinMode(2, OUTPUT);
@@ -34,13 +52,15 @@ void setup() {
   WiFi.begin(ssid, password);
 
   // Wait some time to connect to wifi
-  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++)
+  {
     Serial.print(".");
     delay(1000);
   }
 
   // Check if connected to wifi
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println("Sem Wifi!");
     return;
   }
@@ -50,8 +70,10 @@ void setup() {
   tryConnect();
 
   // run callback when messages are received
-  client.onMessage([&](WebsocketsMessage message) {
+  client.onMessage([&](WebsocketsMessage message)
+                   {
     // Serial.print("Got Message: ");
+    Serial.println(message.data());
     if (message.data() = "true") {
       ionizador = true;
       digitalWrite(2, HIGH);
@@ -62,18 +84,20 @@ void setup() {
       digitalWrite(2, LOW);
       Serial.println(message.data());
       Serial.print("low");
-    }
-  });
+    } });
 }
 
-void loop() {
+void loop()
+{
   // let the websockets client check for incoming messages
   leitura = (analogRead(A0) * 3.3 / 1023);
-  if (client.available()) {
+  if (client.available())
+  {
     client.poll();
-    if (ionizador) client.send("sensor: " + String(leitura) + ",true");
-    else client.send("sensor: " + String(leitura) + ",false");
-  } else {
+    updateServer();
+  }
+  else
+  {
     tryConnect();
   }
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,27 @@ import './App.css';
 import { useState, useEffect, useCallback } from 'react';
 import Container from 'react-bootstrap/Container';
 import useWebSocket from 'react-use-websocket';
+import { ISensors } from './types/ISensors';
+
+
+const inicialSensors: ISensors = {
+  ionizador: { ph: 0, output: undefined, autoStart: { on: false, minValue: 0, maxValue: 0 } }
+}
 
 function App() {
-  const [ionizadorState, setIonizadorState] = useState(false);
-  const [sensorValue, setSensorValue] = useState('');
-  let timer: any;
+  const [sensorState, setSensorState] = useState<ISensors>(inicialSensors);
+  let timer: NodeJS.Timer | undefined;
 
-  const { lastMessage, sendMessage } = useWebSocket('ws://192.168.0.6:3333', {
-    onOpen: () => console.log(`Connected to App WS`),
+  const { lastMessage, sendMessage } = useWebSocket('ws://192.168.0.5:3332', {
+    onOpen: () => { console.log(`Connected to App WS`); sendMessage(`get`) },
     onMessage: () => {
       if (lastMessage) {
         console.log(lastMessage);
 
-        if(lastMessage.data && lastMessage.data.includes('sensor')){
-          const msg = `${lastMessage.data}`.split(': ')[1];
-          setSensorValue(msg.split(',')[0]);
-          setIonizadorState(msg.split(',')[1] === 'true');
-      }
+        if (lastMessage.data && lastMessage.data.includes('server')) {
+          const serverState: ISensors = JSON.parse(`${lastMessage.data}`.split('server: ')[1]);
+          setSensorState({ ...serverState });
+        }
       }
     },
     // queryParams: { 'token': '123456' },
@@ -27,35 +31,33 @@ function App() {
     reconnectInterval: 3000
   });
 
-
-  const senderMessage = () => {
-    timer = !timer && setInterval(() => {
-      console.log('sender');
-      
-      sendMessage(`frontend: ${ionizadorState}`)
-    }, 3000)
-  }
-
   useEffect(() => {
-    senderMessage();
+    if (timer !== undefined) return;
+    timer = setInterval(() => sendMessage(`get`), 3000)
     return () => clearInterval(timer)
-  }, [])
+  }, []);
+
 
   return (
     <Container className="p-3">
       <Container className="p-5 mb-4 bg-light rounded-3" style={{ alignContent: 'center', alignItems: 'center' }}>
         <h1 className="header">Purificador e Phmetro de água</h1>
 
-        <p>Leitura Phmetro: <strong>{sensorValue && sensorValue}</strong></p>
+        <p>Leitura Phmetro: <strong>{sensorState.ionizador.ph}</strong></p>
         <Container>
           <span>Estado do Ionizador: </span>
-          <button className={ionizadorState ? "btn btn-primary" : "btn btn-danger"} type="button" onClick={() => setIonizadorState(!ionizadorState)}>
-            {ionizadorState && <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden={true}></span>}
-            {ionizadorState ? ' Puricando...' : ' Inativo'}
+          <button className={sensorState.ionizador.output ? "btn btn-primary" : "btn btn-danger"} type="button" onClick={() => {
+            const newState = sensorState;
+            newState.ionizador.output = !newState.ionizador.output;
+            sendMessage(`update: ${JSON.stringify(newState)}`)
+            setSensorState(newState);
+          }}>
+            {sensorState.ionizador.output && <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden={true}></span>}
+            {sensorState.ionizador.output ? ' Puricando...' : ' Inativo'}
           </button>
         </Container>
       </Container>
-    </Container>
+    </Container >
   );
 }
 
